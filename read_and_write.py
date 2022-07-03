254e379cd79a207e7b31ea05c81982dd2666971d
Basill <91522891+cherkesovbasil@users.noreply.github.com>
2022-07-03 03:25:30 +0300

    "in progress" upload

--- read_and_write.py
+++ read_and_write.py
@@ -12,6 +12,7 @@ books_read = []
 books_for_refactor = {}
 root = ()
 exceptions_for_translation = ['djvu', 'exe', 'pdf', 'fb2']
+exceptions_for_splitting = ['PDFDrive']
 translated_tag = ''
 
 for root, directory, name in os.walk('C:/Users/vasil/Desktop/мамины книги'):  # считывает все названия из выбранной папки
@@ -35,71 +36,76 @@ for book_name in books_read[0][1]:
 
     # забирает значение названия книги
     pseudo_tags = book_name
-    split_tags = pseudo_tags.replace('.', ' ').split()
 
+    pseudo_tags = pseudo_tags.replace('.', ' ')
+    pseudo_tags = pseudo_tags.replace(',', ' ')
+    pseudo_tags = pseudo_tags.replace('-', ' ')
+    pseudo_tags = pseudo_tags.replace('_', ' ')
+    pseudo_tags = pseudo_tags.replace('(', ' ')
+    pseudo_tags = pseudo_tags.replace(')', ' ')
+    split_tags_with_glued = pseudo_tags.split()
     tags = []
     english_tags = []
 
-    def en_ru(tr_word):
+    # разделяет склееные слова в стиле ПервыеСловаВторыеСлова на рус и en, и фильтрует одиночные символы + слова полностью из верхнего регистра
+    split_tags = []
+    for words_for_splitting in split_tags_with_glued:
+        if words_for_splitting.lower() not in exceptions_for_splitting and len(words_for_splitting) > 1:
+            after_splitting_words = []
+            split_tags.append(words_for_splitting)
+            number_of_upper_symbols = sum(i.isupper() for i in words_for_splitting)
+            if number_of_upper_symbols >= 2 and number_of_upper_symbols != len(words_for_splitting):
+                after_splitting_words.append(re.sub(r'(?<=\w)(?=[A-Z])', '   ', words_for_splitting))
+                after_splitting_words.append(re.sub(r'(?<=\w)(?=[А-Я])', '   ', words_for_splitting))
+                for splits in after_splitting_words:
+                    for items in splits.split():
+                        if len(items) > 1 and items not in split_tags:
+                            split_tags.append(items)
+
+    def en_ru(tr_word='0'):
         """функция перевода на русский и транслитерации"""
 
         global translated_tag
         global all_english_symbols
 
-        # задел для тринслитерации
-        timer = 0
+        # перевод на русский через сервисы гугла
         word = tr_word.lower()
-        print(word + '\n***************without translation***************')
-        for symbol in word:
+        timer = 0
+        for symbol in word.lower():
             if symbol in all_english_symbols:
                 timer += 1
                 if timer == len(word):
                     trans = translator.translate(word, dest='ru')
                     translated_tag = trans.text.lower()
-                    print(translated_tag + 'STOP\n\n\n')
 
         return translated_tag
-            # окончание модуля перевода-транслитерации
 
-    # фильтрация мусора
+
+    # генерация тегов на русском и английском
+    translation = []
+    transliteration = []
+
     for tag in split_tags:
-        print('-----------------START:-----------------\n' + tag)
-        if tag != '(' and tag != ')' and tag != 'PDFDrive' and tag != '_' and tag != '-':
-            if '(' not in tag and ')' not in tag and '_' not in tag and '-' not in tag:
+        if len(tag) > 1:
+            tags.append(tag.lower())
 
-                tags.append(tag.lower())
-                if tag not in exceptions_for_translation:
-                    tr = en_ru(tag)
+            # перевод на английский с проверкой на английские буквы
+            if tag.lower() and tag.isalpha():
+                for alpha in all_english_symbols:
+                    if alpha in tag and tag not in exceptions_for_translation:
+                        tr = en_ru(tag)
+                        if len(tr) > 1:
+                            translation.append(tr)
 
-
-            elif '(' in tag or ')' in tag :
-                clear_tag = re.sub('[(|)]', '', tag)
-                while '(' in clear_tag and ')' in clear_tag:
-                    clear_tag = re.sub('[(|)]', '', tag)
-
-                tags.append(clear_tag.lower())
-                tags.append(tag.lower())
-
-                if clear_tag not in exceptions_for_translation:
-                    tr = en_ru(clear_tag)
-
-
-            if '_' in tag:
-                reformat_tag = re.sub('_', ' ', tag)
-                split_on_words = reformat_tag.replace('_', ' ').split()
-                for words in split_on_words:
-
-                    tags.append(words.lower())
-
-                    if words not in exceptions_for_translation:
-                        tr = en_ru(words)
-
+                            # модуль транслитерации с заменой "W"
+                            transliterated = translit(tag, 'ru').lower()
+                            transliterated = transliterated.replace("w", "в")
+                            if transliterated not in translation:
+                                transliteration.append(transliterated)
+                            break
 
     book_format = tags[-1]
-
-    books_for_refactor[books_read[0][1].index(book_name)] = [root, book_name, book_format, size, local_creation_time, local_modification_time, local_last_open_time, tags]  # прописывает в кортеж номер, директорию и название файла
+    books_for_refactor[books_read[0][1].index(book_name)] = [root, book_name, book_format, size, local_creation_time, local_modification_time, local_last_open_time, tags, translation, transliteration]  # прописывает в кортеж номер, директорию и название файла
 
 for items in books_for_refactor.values():
     print(items)
-
-
